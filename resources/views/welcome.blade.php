b7462ee7927357ffb14949a954ed1c251dd39ba8
fless-lab <achilleatarmla@gmail.com>
2022-03-06 19:22:57 +0000

    Logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="shortcut icon" href="{{ asset("assets/logo-mini.png") }}" type="image/x-icon">
     <title>Carbure +</title>
     <style>
         html {
