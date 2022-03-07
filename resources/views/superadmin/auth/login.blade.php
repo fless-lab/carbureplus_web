1b4d77405962e9a587f315df2a6a4407268fe871
fless-lab <achilleatarmla@gmail.com>
2022-03-07 00:20:28 +0000

    proxies + favicon

--- resources/views/superadmin/auth/login.blade.php
+++ resources/views/superadmin/auth/login.blade.php
@@ -5,6 +5,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="{{ asset("assets/logo-mini.png") }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
