f08a5fdd6081cfa257a2d71dab9aa1a92802fd6f
flesslab <achilleatarmla@gmail.com>
2022-05-31 01:31:24 +0000

    Ajout des autres modifs

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Mail;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+class ContactController extends Controller
+{
+    public function sendMail(Request $request)
+    {
+        $request->validate([
+            "nom" => "required",
+            "email" => "required|email",
+            "description" => "required",
+        ]);
+        $nom = $request->nom;
+        $email = $request->email;
+        $description = $request->description;
+
+        //Load Composer's autoloader
+        require "../../../vendor/autoload.php";
+
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'carbureplus228@example.com';                     //SMTP username
+            $mail->Password   = 'Carbure228Plus';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom($email, $nom);
+            //$mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+            $mail->addAddress('carbureplus228@gmail.com');               //Name is optional
+            //$mail->addReplyTo('info@example.com', 'Information');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
+
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = "Demande d'intégration du service Carbure Plus";
+            $mail->Body    = $description;
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+}
